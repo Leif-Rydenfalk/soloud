fc690fcf1c76adc3b9f93983610b382afe12b378
Kharif <rydenfalk.service@gmail.com>
2024-05-18 10:31:03 +0200

    improved includes

--- include/soloud_core.h
+++ include/soloud_core.h
@@ -5,8 +5,8 @@
 #include "soloud_misc.h"
 #include "soloud_filter.h"
 #include "soloud_bus.h"
-#include "soloud_error.h"
 #include "soloud_audiosource.h"
+#include "soloud_error.h"
 #include "soloud_fader.h"
 #include "soloud_queue.h"
 #include "soloud_thread.h"
@@ -14,15 +14,14 @@
 #include "soloud_file_hack_off.h"
 #include "soloud_file_hack_on.h"
 
+// For testing
+#define ARE_SOLOUD_INCLUDE_ALL
+
 #if defined(ARE_SOLOUD_INCLUDE_AUDIOSOURCES) || defined(ARE_SOLOUD_INCLUDE_ALL)
-#include "soloud_wave.h"
 #include "soloud_wav.h"
 #include "soloud_wavstream.h"
 #include "soloud_speech.h"
 #include "soloud_sfxr.h"
-#include "soloud_beeper.h"
-#include "soloud_modplug.h"
-#include "soloud_prg.h"
 #include "soloud_monotone.h"
 #include "soloud_tedsid.h"
 #endif
@@ -37,7 +36,6 @@
 #include "soloud_robotizefilter.h"
 #include "soloud_freeverbfilter.h"
 #include "soloud_waveshaperfilter.h"
-#include "soloud_phaserfilter.h"
 #endif // ARE_SOLOUD_INCLUDE_EFFECTS
 
 
